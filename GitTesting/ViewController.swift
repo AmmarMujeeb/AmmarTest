4322951fa88baf3e0199244d099d9f563dd7c9cc
ammar <ammar@Ammars-iMac-2.local>
2015-06-16 14:10:38 +0500

    Redo the comment

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-//Added new comment
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
