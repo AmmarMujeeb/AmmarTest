0178843b902d79f67cfce0eeaeba47f7f7fc4fca
ammar <ammar@Ammars-iMac-2.local>
2015-06-16 13:58:34 +0500

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by ammar on 16/06/2015.
+//  Copyright (c) 2015 ammar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
